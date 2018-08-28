1ddca5d8f243cc8153c5ba62489b0c37dcce97c9
John <jmendo12@mai.sfsu.edu>
2018-08-28 12:18:44 -0700

    Added ExcelWriter functionality

--- easybudget/src/main/java/ExcelWriter.java
+++ easybudget/src/main/java/ExcelWriter.java
@@ -1,2 +1,91 @@
+/* This class writes to the user's bank account and expense data to an excel file.
+    Some parts of this class are from this tutorial: https://www.callicoder.com/java-write-excel-file-apache-poi/
+ */
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 public class ExcelWriter {
+
+    private static String[] columns = {"Name", "Total Assets", "Usable Assets", "Total Expenses",
+            "Assets After Payment"};
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        EasyBudget userEB = new EasyBudget();
+        // Get the user's data using the EasyBudget methods
+        userEB.init(sc);
+        userEB.budgetAssets();
+        userEB.budgetExpenses();
+        userEB.payExpenses();
+        System.out.println(userEB.toString());
+
+        //Create an excel workbook
+        Workbook workbook = new XSSFWorkbook();
+        //Used to create instances independent of format
+        CreationHelper creationHelper = workbook.getCreationHelper();
+        //Create an excel sheet
+        Sheet sheet = workbook.createSheet("Buget");
+
+        //Create font for header cells
+        Font headerFont = workbook.createFont();
+        headerFont.setBold(true);
+        headerFont.setFontHeightInPoints((short) 14);
+        headerFont.setColor(IndexedColors.BLACK.getIndex());
+
+        //Create a CellStyle with font
+        CellStyle headerCellStyle = workbook.createCellStyle();
+        headerCellStyle.setFont(headerFont);
+
+        //Create a row
+        Row headerRow = sheet.createRow(0);
+
+        //Create cells
+        for(int i = 0; i < columns.length; i++){
+            Cell cell = headerRow.createCell(i);
+            cell.setCellValue(columns[i]);
+            cell.setCellStyle(headerCellStyle);
+        }
+
+        //Create Cell Style for formatting Date
+        CellStyle dateCellStyle = workbook.createCellStyle();
+        dateCellStyle.setDataFormat(creationHelper.createDataFormat().getFormat("dd-MM-yyyy"));
+
+        //Create other rows and cells with budget data
+        int rowNum = 1;
+        Row row = sheet.createRow(rowNum);
+        //Create cells and populate with user data
+        row.createCell(0).setCellValue(userEB.getUserName());
+        row.createCell(1).setCellValue(userEB.getTotalAssets());
+        row.createCell(2).setCellValue(userEB.getUsableAssets());
+        row.createCell(3).setCellValue(userEB.getTotalExpenses());
+        row.createCell(4).setCellValue(0);
+
+        //Resize all columns to fit the content size
+        for(int i = 0; i < columns.length; i++){
+            sheet.autoSizeColumn(i);
+        }
+
+        //Write output to a file, catch any exceptions
+        try{
+            FileOutputStream fileOut = new FileOutputStream("Budget.xlsx");
+            workbook.write(fileOut);
+            fileOut.close();
+            //Close the workbook
+            workbook.close();
+        }catch(IOException e){
+            e.getStackTrace();
+        }
+    }
 }
